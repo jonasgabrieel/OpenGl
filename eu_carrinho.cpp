5970ed79fb8a41ad32c07e6574f54c9ed2e71df9
Jonas Gabriel <jonasgabrieel@gmai.com>
2024-03-25 16:17:52 -0300

    Musica

--- eu_carrinho.cpp
+++ eu_carrinho.cpp
@@ -67,8 +67,8 @@ void loadCarTexture() {
     glBindTexture(GL_TEXTURE_2D, texNameCarrinho);
 
     // Define os parâmetros de textura
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
